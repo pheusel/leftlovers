71f5a9aa876410c4264b20a70b0c25a52b19bd43
Hassel16 <daniel.hasselwander@nttdata.com>
2018-04-09 10:56:09 +0200

    s

--- ows/src/main/java/dhbw/wpvs/ows/service/WeatherConstants.java
+++
@@ -1,9 +0,0 @@
-package dhbw.wpvs.ows.service;
-
-public class WeatherConstants {
-
-    public static final String FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?q={city},{country}&APPID={key}";
-    public static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={city},{country}&APPID={key}";
-    public static final String APP_ID = "dcadf1aa89a64aaa8ca8bb633b97a2c4";
-
-}
